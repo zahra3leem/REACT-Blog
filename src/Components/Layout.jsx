2e837081e5194214d3e7c37b869258cf3f76ef02
Zahra Abdelaleem <123109918+zahra3leem@users.noreply.github.com>
2024-09-12 16:36:42 +0300

    add navbar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,14 +1,11 @@
 import { Outlet } from "react-router-dom"
-
-
-
-
-
 import Header from "./Header"
+import Navbar from "./Navbar"
 
 function Layout() {
   return (
     <>
+    <Navbar />
       <Header />
        <Outlet />
    
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar= ()=>{
         <>
         <div className="navbar text-zinc-700  p-1">
         <div className="flex-1">
-          <img src={logoo} alt="logo" className="w-20 h-20 bg-red-300" />
+          <img src={logoo} alt="logo" className="w-20 h-20 " />
         </div>
         <div>
           <ul>
