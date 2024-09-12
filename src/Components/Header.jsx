862e68a4bcc5f4efbe18113f70bcebb36520fbda
Zahra Abdelaleem <123109918+zahra3leem@users.noreply.github.com>
2024-09-12 16:31:39 +0300

    fix responsive on landing page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,40 +1,25 @@
-import { Link } from "react-router-dom"
-import logo from "./images/Wall post.gif"
-import logoo from "./images/logoo-removebg-preview.png"
+import logo from "./images/Wall post.gif";
 
 function Header() {
   return (
-    <>
+    <div className="container">
+      
+      <div className="h-[90vh] flex items-center justify-between flex-col md:flex-row   bg-customBlue  rounded-xl p-3">
+    <div className=" w-full md:w-[70%] h-full">
 
-      <div className="navbar text-zinc-700  p-1">
-        <div className="flex-1">
-<img src={logoo} alt="logo" className="w-20 h-20"/>
+        <img
+          className=" object-container  object-left rounded-xl  p-1 h-full"
+          src={logo}
+        ></img>
+    </div>
 
+       <h1 className="text-3xl md:text-5xl py-8 md:px-8 font-bold  text-zinc-700 flex-grow">
+       Hallo, Welcome in Dory blog website.
 
-        
-        </div>
-        <div>
-          <ul>
-            <button className="btn shadow-sm hover:bg-slate-700 focus:outline-none  btn-outline glass sm:btn-sm md:btn-md "><li><Link to="/log">Login</Link></li></button>
-            <button className="btn shadow-sm hover:bg-slate-700 focus:outline-none  btn-outline glass sm:btn-sm md:btn-md "><li><Link to="/register">Register</Link></li></button>
-          </ul>
-        </div>
-      </div>
-
-
-      <div className="flex items-center justify-between container bg-customBlue  rounded-xl md:p-1">
-        <img  className="w-8/12 rounded-xl md:w-8/12 mb-4 md:mb-0 " src={logo}>
-        </img>
-        
-        <h1 className=" w-1/2 text-5xl font-bold  text-zinc-700 ">
-          Hallo, Welcome in Dory blog website.
         </h1>
-
       </div>
-
-    </>
-
-  )
+    </div>
+  );
 }
 
-export default Header
+export default Header;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import logoo from "./images/logoo-removebg-preview.png";
+
+
+const Navbar= ()=>{
+    return (
+        <>
+        <div className="navbar text-zinc-700  p-1">
+        <div className="flex-1">
+          <img src={logoo} alt="logo" className="w-20 h-20 bg-red-300" />
+        </div>
+        <div>
+          <ul>
+            <button className="btn shadow-sm hover:bg-slate-700 focus:outline-none  btn-outline glass sm:btn-sm md:btn-md ">
+              <li>
+                <Link to="/log">Login</Link>
+              </li>
+            </button>
+            <button className="btn shadow-sm hover:bg-slate-700 focus:outline-none  btn-outline glass sm:btn-sm md:btn-md ">
+              <li>
+                <Link to="/register">Register</Link>
+              </li>
+            </button>
+          </ul>
+        </div>
+      </div>
+      </>
+
+    )
+}
+
+export default Navbar
